1a8b0a60f70d89f1c3e7c3117684d35d2f22cb26
bahodirovweb <bohodirovweb@gmail.com>
2024-03-02 14:45:24 +0500

    init commit

---
+++ js/fn.js
@@ -0,0 +1,9 @@
+function fn() {
+    if (value == 'katta') {
+        elSpan.textContent = 'katta';
+    } if (value == 'ortacha') {
+        elSpan.textContent = 'ortacha';
+    } if (value == 'kichik') {
+        elSpan.textContent = 'kichik';
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,46 @@
+var elSelect = document.querySelector('.form-select')
+var elSpan = document.querySelector('.span')
+var elSpan1 = document.querySelector('.spa')
+var elSpan2 = document.querySelector('.sp')
+var elSpan = document.querySelector('.s')
+var elInput = document.querySelector('.ch')
+var elInput = document.querySelector('.c')
+/*function fn(){
+    if (value  == 'katta') {
+        elSpan.textContent = 'katta'
+    } if (value == 'ortacha') {
+        elSpan.textContent = 'ortacha'
+    } if (value  == 'kichik') {
+        elSpan.textContent = 'kichik'
+    }
+}*/
+function ta(){
+    elSpan.textContent = 'katta'
+}
+function a(){
+    elSpan.textContent ='orta'
+}
+function t(){
+    elSpan.textContent = 'kichik'
+}
+
+function bt() {
+    elSpan1.textContent = '25cm'
+}
+function b() {
+    elSpan1.textContent = '30cm'
+}
+function t() {
+    elSpan1.textContent = '35cm'
+}
+
+function imp() {
+    elSpan2.textContent = 'Pamidor'
+}
+
+function ch(){
+    elSpan.textContent = 'Achik'
+}
+function c(){
+    elSpan.textContent = 'Shirin'
+}
